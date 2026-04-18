98b498834f8619caeec9b305c1ff49a61a36a155
alexeyzayac <alexeyzayac@icloud.com>
2026-04-18 20:44:50 +0300

    0.0.61

---
+++ Terraform/Docker_Compose/terraform/06_vm-docker-test.tf
@@ -0,0 +1,40 @@
+# 06_vm-docker-test.tf
+
+# vm-swarm-manager
+resource "yandex_compute_instance" "vm-docker-test" {
+  name        = "vm-docker-test"
+  hostname    = "vm-docker-test"
+  platform_id = "standard-v3"
+  zone        = "ru-central1-a"
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.toolbox.image_id
+      type     = "network-hdd"
+      size     = 30
+    }
+  }
+
+  metadata = {
+    user-data = templatefile("../cloud-init/cloud-init.tpl", {
+      public_key = tls_private_key.ssh.public_key_openssh
+    })
+    serial-port-enable = 1
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.subnet_a.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.docker_sg.id]
+  }
+}
